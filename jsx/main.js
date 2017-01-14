dc77e1934a34e594ba448958ae8f0e45848a37ae
Sebastian Proost <sebastian.proost@gmail.com>
2017-01-14 18:56:17 +0100

    integrating rebass

--- jsx/main.js
+++ jsx/main.js
@@ -1,23 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import {Container, Card, Heading, Switch} from 'rebass';
+
 const app = document.getElementById('motiongui');
 
 
 class App extends React.Component {
   render() {
     return (
-      <div>
-      <div className="card">
-        <h1>Motion-GUI</h1>
-      </div>
-        <div>
-            image
-        </div>
-        <div className="card">
+      <Container style="width:256, max-width:256">
+          <Card rounded width={256}>
+            <Heading level={1}>Motion-GUI</Heading>
+            <Switch /><Switch checked />
             <a href={this.props.start_url}>start</a> <a href={this.props.stop_url}>stop</a>
-        </div>
-      </div>
+          </Card>
+      </Container>
     );
   }
 }
